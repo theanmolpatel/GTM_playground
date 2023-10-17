94472c95b5ae9f6c2f0b76f5d8b7a201ac225f53
AdamParker19 <anmol.patel@sportsbuzz11.com>
2023-10-18 01:18:46 +0530

    setup project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SignIn from './components/Signin';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <SignIn></SignIn>
+      </body>
+    </div>
+  );
+}
+
+export default App;
