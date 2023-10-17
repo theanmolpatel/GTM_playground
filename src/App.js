36d68a7e422e9f60960d551d033a0fd04b511fe4
AdamParker19 <anmol.patel@sportsbuzz11.com>
2023-10-18 03:01:27 +0530

    added google tag manager

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import SignIn from './components/Signin';
-
+import TagManager from 'react-gtm-module';
+ 
+TagManager.initialize({ gtmId: `${process.env.REACT_APP_GTM_ID}` });
 function App() {
   return (
     <div className="App">
