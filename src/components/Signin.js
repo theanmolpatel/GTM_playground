70503428208195697fc4148c503afcb71116c0c3
AdamParker19 <anmol.patel@sportsbuzz11.com>
2023-10-19 01:11:04 +0530

    gtm event trigger

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -4,21 +4,22 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+import link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
@@ -33,6 +34,7 @@ export default function SignIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    window.dataLayer.push({ 'event': 'submit'})
     console.log({
       email: data.get('email'),
       password: data.get('password'),
@@ -92,14 +94,14 @@ export default function SignIn() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <link href="#" variant="body2">
                   Forgot password?
-                </Link>
+                </link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <link href="#" variant="body2">
                   {"Don't have an account? Sign Up"}
-                </Link>
+                </link>
               </Grid>
             </Grid>
           </Box>
